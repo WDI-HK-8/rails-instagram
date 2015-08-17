ae230003b6db3500aeb69b1b4a8cd63bc1426fce
Harry Chen <harry@harry.gg>
2015-08-17 13:59:49 +0800

    feat(notes): Add basic Post CRUD with Model and Migrations

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,61 @@
+class PostsController < ApplicationController
+  # before_action :authenticate_user!
+
+  def index
+    @posts = Post.includes(:comments) # retrieve all the Post data, and store them in the variable @posts
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    @post.save
+  end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+
+    if @post.nil?
+      render json: {
+        message: "Cannot find post"
+      }, status: :not_found
+    else
+      @post.update(post_params)
+    end
+  end
+
+  def show
+    @post = Post.find_by_id(params[:id])
+    
+    if @post.nil?
+      render json: {
+        message: "Cannot find post"
+      }, status: :not_found
+    end
+  end
+
+  def destroy
+    @post = Post.find_by_id(params[:id])
+
+    if @post.nil?
+      render json: {
+        message: "Cannot find post"
+      }, status: :not_found
+    else
+      if @post.destroy
+        render json: {
+          message: "Successfully deleted"
+        }, status: :no_content
+      else
+        render json: {
+          message: "Unsuccessfully deleted"
+        }, status: :bad_request
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category, :picture)
+  end
+end
