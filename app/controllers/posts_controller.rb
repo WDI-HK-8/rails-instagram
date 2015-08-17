855f3bfd2c5b66dcc230b73a46e5bfbad6e206bd
Harry Chen <harry@harry.gg>
2015-08-17 19:36:18 +0800

    feat(notes): Add controller validation to make sure that user is authenticated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     @posts = Post.all # retrieve all the Post data, and store them in the variable @posts
