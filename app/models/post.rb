99adfc18bf441fcb44643463bf6e9bf0d059d81c
Harry Chen <harry@harry.gg>
2015-08-24 18:24:35 +0800

    feat(image-upload): Add corresponding configs to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  has_attached_file :image, styles: {
+    medium: "300x300>",
+    thumb: "100x100>"
+  }
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
